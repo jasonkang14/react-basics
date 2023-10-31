8fd2851fada3797905606c4eafd028dd34f314ab
Jason Kang <jasonkang14@gmail.com>
2023-10-31 21:33:51 +0900

    Add storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-designs", // 👈 Addon is registered here
   ],
   framework: {
     name: "@storybook/react-vite",
