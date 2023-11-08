57cd5dd1cd95bcb792ae26af22cd1dc5da098cd7
Jason Kang <jasonkang14@gmail.com>
2023-11-08 15:38:45 +0900

    complete given

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,3 +1,35 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import SignupPage from "../pages/SignupPage";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {},
+});
+
 describe("회원가입 테스트", () => {
-  test("환경설정 테스트", () => {});
+  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다", () => {
+    // given - 회원가입 페이지가 그려짐
+    const routes = [
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/signup"],
+      initialIndex: 0,
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    );
+
+    // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+    // then - 에러메세지가 표시됨
+  });
 });
