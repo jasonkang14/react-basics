e582281f1c1f5dfcdc79211ff6349951c37bb8f9
Jason Kang <jasonkang14@gmail.com>
2023-11-15 15:41:51 +0900

    update cypress settings to use cypress dashboard

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "jqdjrg",
   e2e: {
     baseUrl: "http://localhost:5173",
-  },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
   },
 });
