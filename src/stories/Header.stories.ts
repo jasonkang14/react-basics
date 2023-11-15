060d2741a295ea15491b90bf32e7d2390e0a07ab
Jason Kang <jasonkang14@gmail.com>
2023-11-15 14:35:32 +0900

    Add options to header

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -1,15 +1,18 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Header } from './Header';
+import { Header } from "./Header";
 
 const meta = {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
+  },
+  argTypes: {
+    btnBackgroundColor: { control: "color" },
   },
 } satisfies Meta<typeof Header>;
 
@@ -19,7 +22,7 @@ type Story = StoryObj<typeof meta>;
 export const LoggedIn: Story = {
   args: {
     user: {
-      name: 'Jane Doe',
+      name: "Jane Doe",
     },
   },
 };
