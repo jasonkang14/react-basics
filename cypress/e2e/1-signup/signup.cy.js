6832afdcea6008e7986a348de495624f6b72c70f
Jason Kang <jasonkang14@gmail.com>
2023-11-19 17:35:36 +0900

    Add test condition for signup

--- cypress/e2e/1-signup/signup.cy.js
+++ cypress/e2e/1-signup/signup.cy.js
@@ -6,7 +6,9 @@ describe("회원가입 화면", () => {
         cy.get('[data-cy=emailInput]').as('emailInput');
         cy.get('[data-cy=passwordInput]').as('passwordInput');
         cy.get('[data-cy=confirmPasswordInput]').as('confirmPasswordInput');
+        cy.get('[data-cy=signupButton]').as('signupButton');
 
+        cy.get('@signupButton').should('be.disabled');
 
         // when - 아이디와 비밀번호, 비밀번호 확인값을 입력한다.
         cy.get('@emailInput').type('test@email.com');
@@ -15,9 +17,12 @@ describe("회원가입 화면", () => {
 
         cy.get('@emailInput').invoke('val').should('eq', 'test@email.com');
         cy.get('@passwordInput').invoke('val').should('eq', 'password');
+
+        // 비밀번호와 비밀번호 확인값이 일치하는지 확인한다.
         cy.get('[data-cy=passwordInput]').invoke('val').then((value1) => {
             cy.get('[data-cy=confirmPasswordInput]').invoke('val').then((value2) => {
-              expect(value1).to.equal(value2);
+                expect(value1).to.equal(value2);
+                cy.get('@signupButton').should('not.be.disabled');
             });
           });
           
