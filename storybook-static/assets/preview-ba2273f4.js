bcc3fb0a8facf145d28a3560c12be79ce091733b
Jason Kang <jasonkang14@gmail.com>
2024-01-05 19:20:06 +0900

    Remove test

--- storybook-static/assets/preview-ba2273f4.js
+++
@@ -1,2 +0,0 @@
-const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{e as default};
-//# sourceMappingURL=preview-ba2273f4.js.map
