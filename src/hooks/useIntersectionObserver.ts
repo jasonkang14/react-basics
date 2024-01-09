af2bde0c3d63df2de8f7a78ee69111350f1fab60
Jason Kang <jasonkang14@gmail.com>
2024-01-09 18:25:12 +0900

    Add custom hook to utilize intersection observer api

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect, RefObject } from "react";
+
+function useIntersectionObserver<T extends HTMLElement>(ref: RefObject<T>): boolean {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1, 
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref]);
+
+  return isVisible;
+}
+
+export default useIntersectionObserver;
